bac57b2c01b76ea05c8f4fa2079030a972cb2e07
santinoMatIm <santinomi1306@gmail.com>
2026-04-10 13:25:30 -0600

    failed test

--- sample-app/app.js
+++ sample-app/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hola desde la app de demostración');
+  res.send('Hola desde la app');
 });
 
 app.get('/saludo/:nombre', (req, res) => {
